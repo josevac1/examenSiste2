f500614fcfa3d6bba69fe17426f8daf84582a732
josevac1 <josevanegas@gmail.com>
2026-07-28 17:45:39 -0500

    ultima falla

--- test/server.test.js
+++ test/server.test.js
@@ -5,6 +5,6 @@ test("GET / responde correctamente", async () => {
   const response = await fetch("http://localhost:8080/");
   const body = await response.text();
 
-  assert.equal(response.status, 401);
+  assert.equal(response.status, 200);
   assert.match(body, /Aplicación lista/);
 });
